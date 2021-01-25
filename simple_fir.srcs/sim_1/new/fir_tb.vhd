f9d4836c2b4baf560d4a214f61635a806d36055c
sympathyvote <53431614+sympathyvote@users.noreply.github.com>
2021-01-26 00:18:33 +0100

    0.3.0a
    added scaler component, added phase_shifter component, added in-quadrature path

--- simple_fir.srcs/sim_1/new/fir_tb.vhd
+++ simple_fir.srcs/sim_1/new/fir_tb.vhd
@@ -49,6 +49,31 @@ component mixer
     );
 end component;
 
+component scaler
+    generic (
+        INPUT_RESOLUTION : integer;
+        OUTPUT_RESOLUTION : integer;
+        DIVISION_FACTOR : std_logic_vector
+        );
+    Port (
+        data_in : in signed(INPUT_RESOLUTION-1 downto 0);
+        data_out : out signed(OUTPUT_RESOLUTION-1 downto 0)
+    );
+end component;
+
+component phase_shifter
+    Generic (
+           INPUT_RESOLUTION : integer;
+           SAMPLES_DELAY : integer
+           );
+    Port ( 
+           clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           data_in : in signed(INPUT_RESOLUTION - 1 downto 0);
+           data_out : out signed(INPUT_RESOLUTION - 1 downto 0)
+           );
+end component;
+
   -- common
   signal clk: std_logic;
   signal en: std_logic;
@@ -57,14 +82,30 @@ end component;
   
   -- fir1
   signal data_in: signed(7 downto 0); -- also system in
+  signal fir1_to_mixer: signed(14 downto 0);
 
   -- mixer
-  signal fir1_to_mixer: signed(14 downto 0);
   signal ref_in: signed(14 downto 0);
+  signal mixer_to_fir2: signed(28 downto 0);
   
   -- fir2
-  signal mixer_to_fir2: signed(28 downto 0);
-  signal data_out: signed(56 downto 0); -- also system out 
+  signal fir2_to_scaler: signed(56 downto 0);
+  
+  -- scaler
+  signal data_out: signed(15 downto 0);
+  
+  -- phase shifter
+  signal phase_shifter_to_mixer_iq: signed(14 downto 0);
+  
+  -- mixer in quadrature
+  signal mixer_iq_to_fir2_iq: signed(28 downto 0);
+  
+  -- fir2 in quadrature
+  signal fir2_iq_to_scaler_iq: signed(56 downto 0);
+  
+  -- scaler
+  signal data_out_iq: signed(15 downto 0);
+   
 
   constant clock_period: time := 20 ns;
   signal stop_the_clock: boolean;
@@ -75,10 +116,12 @@ end component;
    constant filename_in: string := "C:\Users\Win81\Vivado\Projects\simple_fir\data_in.txt";
    constant filename_lo: string := "C:\Users\Win81\Vivado\Projects\simple_fir\lo_in.txt";
    constant filename_out: string := "C:\Users\Win81\Vivado\Projects\simple_fir\data_out.txt";
+   constant filename_out_iq: string := "C:\Users\Win81\Vivado\Projects\simple_fir\data_out_iq.txt";
    
    file fptr_in : text;
    file fptr_lo : text;
    file fptr_out : text;
+   file fptr_out_iq : text;
 
 begin
 
@@ -123,13 +166,73 @@ begin
                b5   => 45622067,
                b6   => 29616455,
                b7   => 8723754
-  )
+               )
   port map (clk      => clk,
             data_in  => mixer_to_fir2,
-            data_out => data_out,
+            data_out => fir2_to_scaler,
             en       => en,
             start    => start,
-            reset    => reset );                      
+            reset    => reset );
+                      
+            
+  scaler1: scaler
+  generic map(
+            INPUT_RESOLUTION => 57,
+            OUTPUT_RESOLUTION => 16,
+            DIVISION_FACTOR => "011111111111111111111111111111111111111111"
+            )
+  port map (data_in     => fir2_to_scaler,
+            data_out    => data_out);
+            
+  phase_shifter1: phase_shifter
+  generic map(
+            INPUT_RESOLUTION => 15,
+            SAMPLES_DELAY => 1
+            )
+  port map(
+            data_in => ref_in,
+            data_out => phase_shifter_to_mixer_iq,
+            clk => clk,
+            reset => reset
+           );   
+          
+  mixer_iq: mixer
+  port map (    data_in     => fir1_to_mixer,
+                ref_in      => phase_shifter_to_mixer_iq,
+                data_out    => mixer_iq_to_fir2_iq,
+                clk => clk 
+                );
+                
+  fir2_iq: fir 
+  generic map (
+               INPUT_RESOLUTION => 29,
+               OUTPUT_RESOLUTION => 57,
+                    
+               b0   => 8723754,
+               b1   => 29616455,
+               b2   => 45622067,
+               b3   => 59828855,
+               b4   => 59828855,
+               b5   => 45622067,
+               b6   => 29616455,
+               b7   => 8723754
+               )
+  port map (clk      => clk,
+            data_in  => mixer_iq_to_fir2_iq,
+            data_out => fir2_iq_to_scaler_iq,
+            en       => en,
+            start    => start,
+            reset    => reset );
+            
+              scaler_iq: scaler
+  generic map(
+            INPUT_RESOLUTION => 57,
+            OUTPUT_RESOLUTION => 16,
+            DIVISION_FACTOR => "011111111111111111111111111111111111111111"
+            )
+  port map (data_in     => fir2_iq_to_scaler_iq,
+            data_out    => data_out_iq);                            
+                                  
 
   stimulus: process
   
@@ -170,6 +273,7 @@ begin
     file_open(fstatus, fptr_in, filename_in, read_mode);
     file_open(fstatus, fptr_lo, filename_lo, read_mode);
     file_open(fstatus, fptr_out, filename_out, write_mode);
+    file_open(fstatus, fptr_out_iq, filename_out_iq, write_mode);
     
     -- zdecydowaæ czy pierwsza iteracja oddzielnie, ¿eby zapisywane dane do pliku nie ³apa³y niepotrzebnego zera
     wait for 10 ns;
@@ -190,9 +294,12 @@ begin
         read(file_line, lo_signal);
         ref_in <= to_signed(lo_signal,15);
         
-        
+        -- write output data to file
         write(file_line, std_logic_vector(data_out), left, 5); 
         writeline(fptr_out, file_line);
+        
+        write(file_line, std_logic_vector(data_out_iq), left, 5); 
+        writeline(fptr_out_iq, file_line);
     end loop;
     
     -- keep writing values till filter response zeroes
